7f248808a68f680fada2b2c57b2b4657dba7a4f9
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-09-10 18:39:33 +0900

    feat: CardList 컴포넌트, 테스트 파일 추가

---
+++ components/card/CardList.tsx
@@ -0,0 +1,15 @@
+import { Gathering } from '@/types/types';
+import Card from './Card';
+import React from 'react';
+
+const CardList = ({ gatherings }: { gatherings: Gathering[] }) => {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      {gatherings.map((gathering) => (
+        <Card gathering={gathering} normal={false} key={gathering.id} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
