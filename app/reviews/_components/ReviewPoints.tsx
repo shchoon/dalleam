c1a75127ed49f7e0296a30eb9742e94bbc98df99
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-09-24 23:49:52 +0900

    Feat: 애니메이션 추가

--- app/reviews/_components/ReviewPoints.tsx
+++ app/reviews/_components/ReviewPoints.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import FillHeart from '/public/icons/fill_heart.svg';
+'use client';
 
+import React, { useEffect } from 'react';
+import FillHeart from '/public/icons/fill_heart.svg';
+import { useMotionValue, useTransform, animate, motion } from 'framer-motion';
 
 export default function ReviewPoints() {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+
+  useEffect(() => {
+    const controls = animate(count, 4);
+    return () => controls.stop();
+  }, []);
+
   return (
     <div className="flex w-full h-180pxr justify-center shrink-0 bg-white border-y-2 border-gray-200 my-5">
       <div className="flex h-full w-294pxr justify-between md:w-full md:gap-120pxr md:justify-center items-center">
         {/* 하트 */}
         <div className="flex flex-col items-center gap-2">
-          <div className="flex items-start gap-2pxr">
-            <span className="text-2xl font-semibold text-gray-900">4.0</span>
+          <div className="flex items-start gap-2pxr text-2xl font-semibold text-gray-900">
+            <motion.span>{rounded}</motion.span>.0
             <span className="text-2xl font-semibold text-gray-400">/5</span>
           </div>
           <div className="flex items-start gap-2pxr">
-            <FillHeart className="text-red-500" />
-            <FillHeart className="text-red-500" />
-            <FillHeart className="text-red-500" />
-            <FillHeart className="text-red-500" />
-            <FillHeart className="text-gray-400" />
+            <FillHeart className="text-red-500 animate-fFill-heart" />
+            <FillHeart className="text-red-500 animate-fFill-heart" />
+            <FillHeart className="text-red-500 animate-fFill-heart" />
+            <FillHeart className="text-red-500 animate-fFill-heart" />
+            <FillHeart className="text-gray-400 animate-fFill-heart" />
           </div>
         </div>
         {/* 그래프 */}
@@ -26,7 +36,13 @@ export default function ReviewPoints() {
             <div className="flex items-center gap-3">
               <span className="text-sm font-medium text-gray-700">5점</span>
               <span className="w-84pxr lg:w-60 h-1 bg-gray-300 rounded-md relative">
-                <span className="absolute top-0 left-0 w-70pxr lg:w-52 h-1 bg-black"></span>
+                <motion.span
+                  initial={{ scaleX: 0 }} // 초기 값: scaleX 0 (너비 0)
+                  animate={{ scaleX: 1 }} // 목표 값: scaleX 1 (너비 100%)
+                  transition={{ duration: 0.8 }} // 애니메이션 지속 시간
+                  style={{ transformOrigin: 'left' }} // 왼쪽을 기준으로 확장
+                  className="absolute top-0 left-0 lg:w-52 h-1 bg-black"
+                ></motion.span>
               </span>
             </div>
             <span className="text-sm font-medium text-gray-400">27</span>
@@ -36,7 +52,13 @@ export default function ReviewPoints() {
             <div className="flex items-center gap-3">
               <span className="text-sm font-medium text-gray-700">4점</span>
               <span className="w-84pxr lg:w-60 h-1 bg-gray-300 rounded-md relative">
-                <span className="absolute top-0 left-0 w-60pxr lg:w-52 h-1 bg-black"></span>
+                <motion.span
+                  initial={{ scaleX: 0 }} // 초기 값: scaleX 0 (너비 0)
+                  animate={{ scaleX: 1 }} // 목표 값: scaleX 1 (너비 100%)
+                  transition={{ duration: 0.8 }} // 애니메이션 지속 시간
+                  style={{ transformOrigin: 'left' }} // 왼쪽을 기준으로 확장
+                  className="absolute top-0 left-0 lg:w-60pxr h-1 bg-black"
+                ></motion.span>
               </span>
             </div>
             <span className="text-sm font-medium text-gray-400">19</span>
@@ -46,7 +68,13 @@ export default function ReviewPoints() {
             <div className="flex items-center gap-3">
               <span className="text-sm font-medium text-gray-700">3점</span>
               <span className="w-84pxr lg:w-60 h-1 bg-gray-300 rounded-md relative">
-                <span className="absolute top-0 left-0 w-2 lg:w-3 h-1 bg-black"></span>
+                <motion.span
+                  initial={{ scaleX: 0 }} // 초기 값: scaleX 0 (너비 0)
+                  animate={{ scaleX: 1 }} // 목표 값: scaleX 1 (너비 100%)
+                  transition={{ duration: 0.8 }} // 애니메이션 지속 시간
+                  style={{ transformOrigin: 'left' }} // 왼쪽을 기준으로 확장
+                  className="absolute top-0 left-0 lg:w-3 h-1 bg-black"
+                ></motion.span>
               </span>
             </div>
             <span className="text-sm font-medium text-gray-400">2</span>
--- app/reviews/_components/ReviewsTabs.tsx
+++ app/reviews/_components/ReviewsTabs.tsx
@@ -1,52 +1,113 @@
+'use client';
+
 import Chip from '@/components/chip/Chip';
-import React from 'react';
+import React, { useState } from 'react';
 import DallaemFitIcon from '/public/icons/dallaem_fit_icon.svg';
 import Workation from '/public/icons/workation_icon.svg';
+import TabsUnderline from '/public/icons/tabs_underline.svg';
 import Stroke from '/public/icons/stroke.svg';
+import clsx from 'clsx';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function ReviewsTabs() {
+  const [mainTab, setMainTab] = useState<'달램 핏' | '워케이션'>('달램 핏');
+  const [subTab, setSubTab] = useState<'전체' | '오피스 스트레칭' | '마인드풀니스'>('전체');
+
   return (
-    <div className="inline-flex flex-col w-full gap-3 items-start">
-      <div className="flex items-start gap-3">
+    <div className="inline-flex flex-col w-full gap-3 items-start md:mb-2">
+      <div className="flex items-start gap-3 relative mb-2">
         <div className="flex flex-col items-start gap-2">
-          <div className="flex items-center gap-2">
-            <span className="text-lg font-semibold">달램 핏</span>
-            <DallaemFitIcon />
-          </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="83"
-            height="2"
-            viewBox="0 0 83 2"
-            fill="none"
+          <div
+            onClick={() => setMainTab('달램 핏')}
+            className="flex items-center gap-1 cursor-pointer"
           >
-            <path
-              d="M0 1C0 0.447715 0.447715 0 1 0H82C82.5523 0 83 0.447715 83 1C83 1.55228 82.5523 2 82 2H0.999999C0.447715 2 0 1.55228 0 1Z"
-              fill="#111827"
+            <span
+              className={clsx(
+                mainTab === '달램 핏' && 'text-lg font-semibold',
+                'transition-all duration-500 linear',
+              )}
+            >
+              달램 핏
+            </span>
+            <DallaemFitIcon
+              className={clsx(
+                mainTab === '달램 핏' ? 'text-gray-900' : 'text-gray-400',
+                'transition-colors duration-500 linear',
+              )}
             />
-          </svg>
-        </div>
-        <div className="flex items-center gap-2">
-          <span className="text-lg font-semibold text-gray-400">워케이션</span>
-          <Workation />
-        </div>
-      </div>
-      <div className="flex flex-col w-full items-start gap-3">
-        <div className="flex flex-col items-start gap-4 self-stretch">
-          <div className="flex items-start gap-2">
-            <Chip color="navy" size="lg">
-              전체
-            </Chip>
-            <Chip color="gray" size="lg">
-              오피스 스트레칭
-            </Chip>
-            <Chip color="gray" size="lg">
-              마인드풀니스
-            </Chip>
           </div>
-          <Stroke className="w-full" />
         </div>
+
+        <div
+          onClick={() => setMainTab('워케이션')}
+          className="flex items-center gap-1 cursor-pointer"
+        >
+          <span
+            className={clsx(
+              mainTab === '워케이션' && 'text-lg font-semibold',
+              'transition-all duration-500 linear',
+            )}
+          >
+            워케이션
+          </span>
+          <Workation
+            className={clsx(
+              mainTab === '워케이션' ? 'text-gray-900' : 'text-gray-400',
+              'transition-colors duration-500 linear',
+            )}
+          />
+        </div>
+
+        {/* TabsUnderline 애니메이션 추가 */}
+        <TabsUnderline
+          className={clsx(
+            'absolute -bottom-2 h-1 transition-transform duration-300 linear',
+            mainTab === '달램 핏'
+              ? 'w-20 transform translate-x-0'
+              : 'w-100pxr transform translate-x-full',
+          )}
+        />
       </div>
+      <AnimatePresence>
+        {mainTab === '달램 핏' && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex flex-col w-full items-start gap-3"
+          >
+            <div className="flex flex-col items-start gap-4 self-stretch">
+              <div className="flex items-start gap-2">
+                <Chip
+                  className="transition-colors duration-500 linear"
+                  onClick={() => setSubTab('전체')}
+                  color={subTab === '전체' ? 'navy' : 'gray'}
+                  size="lg"
+                >
+                  전체
+                </Chip>
+                <Chip
+                  className="transition-colors duration-500 linear"
+                  onClick={() => setSubTab('오피스 스트레칭')}
+                  color={subTab === '오피스 스트레칭' ? 'navy' : 'gray'}
+                  size="lg"
+                >
+                  오피스 스트레칭
+                </Chip>
+                <Chip
+                  className="transition-colors duration-500 linear"
+                  onClick={() => setSubTab('마인드풀니스')}
+                  color={subTab === '마인드풀니스' ? 'navy' : 'gray'}
+                  size="lg"
+                >
+                  마인드풀니스
+                </Chip>
+              </div>
+              <Stroke className="w-full" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
