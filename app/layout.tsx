48862ba96358d914e8af595969b90cd30093e5e7
ohdong9795 <ohdong9795@naver.com>
2024-09-27 14:28:17 +0900

    feat: add NavBar, Dropdown components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import QueryProvider from '@/components/QueryProvider';
 import './globals.css';
 import localFont from 'next/font/local';
+import NavBar from '@/components/nav/NavBar';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -15,14 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${pretendard.className} relative`}>
-        <div className="flex flex-col w-full">
-          <div className="w-full text-3xl text-center bg-orange-600 h-56pxr md:h-60pxr">
-            네비게이션 들어가는 곳
-          </div>
-          <QueryProvider>
-            <div className="bg-gray-100 flex items-center flex-col">{children}</div>
-          </QueryProvider>
+      <body className={pretendard.className}>
+        <div className="flex flex-col w-full relative">
+          <NavBar />
+          <QueryProvider>{children}</QueryProvider>
         </div>
         <div id="global-modal"></div>
       </body>
---
+++ components/nav/NavMenu.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  { name: '모임 찾기', href: '/' },
+  { name: '찜한 모임', href: '/favorites' },
+  { name: '모든 리뷰', href: '/reviews' },
+];
+
+const isSelected = (currentPathname: string, href: string) => currentPathname === href;
+
+export default function NavMenu() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link) => (
+        <Link key={link.href} href={link.href}>
+          <span className={isSelected(pathname, link.href) ? 'text-gray-900' : ''}>
+            {link.name}
+          </span>
+        </Link>
+      ))}
+    </>
+  );
+}
