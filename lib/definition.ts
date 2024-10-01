4c43c0f13e4583438942cdc7c04d60e4499f7f0c
shcho <acckr0604@gmail.com>
2024-10-01 17:32:35 +0900

    feat: container 호버 위치, couner animation 수정

--- lib/definition.ts
+++ lib/definition.ts
@@ -39,3 +39,17 @@ export type Review = {
     image: string;
   };
 };
+
+export type Participant = {
+  teamId: string;
+  userId: number;
+  gatheringId: number;
+  joinedAt: string;
+  User: {
+    id: number;
+    email: string;
+    name: string;
+    companyName: string;
+    image: string | null;
+  };
+};
