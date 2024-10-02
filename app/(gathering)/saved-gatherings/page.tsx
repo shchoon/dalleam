0d2ac489a6aaa3b2ca0a276897427918c7e097d5
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-10-02 16:22:35 +0900

    feat: 상세 및 찜 목록 페이지 추가

---
+++ app/(gathering)/saved-gatherings/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Heart from '/public/icons/type_saved.svg';
+
+import CommonFilterSection from '../_components/CommonFilterSection';
+import SavedProgressCardList from '../_components/SavedProgressCardList';
+
+const savedGatheringPage = () => {
+  return (
+    <div className="h-full px-4 pt-6 min-h-dvh bg-gray-50 pb-12pxr max-w-1200pxr w-375pxr md:w-744pxr md:pl-6 md:pr-25pxr md:pb-45pxr md:pt-46pxr lg:w-1200pxr lg:px-102pxr lg:pt-41pxr">
+      <div className="flex flex-col">
+        <div className="inline-flex items-center gap-4">
+          <Heart />
+          <div className="flex flex-col gap-2">
+            <span className="text-2xl font-semibold text-gray-900">찜한 모임</span>
+            <span className="text-sm font-medium text-gray-700">
+              마감되기 전에 지금 바로 참여해보세요 👀
+            </span>
+          </div>
+        </div>
+        <CommonFilterSection />
+        <SavedProgressCardList />
+      </div>
+    </div>
+  );
+};
+
+export default savedGatheringPage;
