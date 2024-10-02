1e650808e51043eeb8a7b7a3a68743d7a1dc2cfc
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-10-02 13:37:25 +0900

    fix: 옵저버 훅 수정

--- hooks/useInfiniteObserver.ts
+++ hooks/useInfiniteObserver.ts
@@ -2,29 +2,28 @@ import { useCallback, useEffect, useRef } from 'react';
 
 export const useInfiniteObserver = (callback: () => void, options?: IntersectionObserverInit) => {
   const ref = useRef<HTMLDivElement>(null);
+
   const intersectionObserverCallback = useCallback(
-    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-      console.log('call');
-      console.log('entries =', entries);
+    (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          observer.unobserve(entry.target);
-          callback();
+          callback(); // 교차할 때 콜백 호출
         }
       });
     },
-    [callback],
+    [callback], // callback이 변경될 때만 새로 생성
   );
 
   useEffect(() => {
-    console.log('ref = ', ref);
-    if (!ref.current) return;
-
+    const target = ref.current;
+    if (!target) return;
     const observer = new IntersectionObserver(intersectionObserverCallback, options);
-    observer.observe(ref.current);
-
-    return () => observer.disconnect();
-  }, [ref]);
+    observer.observe(target);
+    return () => {
+      if (target) observer.unobserve(target);
+      observer.disconnect();
+    };
+  }, [intersectionObserverCallback, options, ref.current]);
 
   return ref;
 };
