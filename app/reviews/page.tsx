e7777ed6e587672d29339db11178cb7e6d679ff2
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-10-02 13:40:52 +0900

    feat: SSR 적용

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,8 +1,16 @@
+// app/reviews/page.tsx
 import React from 'react';
 import Reviews from './_components/Reviews';
+import { HydrationBoundary } from '@tanstack/react-query';
+import { dehydrate } from '@tanstack/react-query';
+import { useReviewPrefetchQuery } from '@/services/reviews';
 
-function page() {
-  return <Reviews />;
+export default async function Page() {
+  const queryClient = await useReviewPrefetchQuery();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Reviews />
+    </HydrationBoundary>
+  );
 }
-
-export default page;
