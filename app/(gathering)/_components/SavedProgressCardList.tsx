4730ec4d1d029cf837105ea42d88e440c68fa05d
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-10-08 16:49:39 +0900

    feat: ProgressCardList 컴포넌트 스크롤 애니메이션 추가

--- app/(gathering)/_components/SavedProgressCardList.tsx
+++ app/(gathering)/_components/SavedProgressCardList.tsx
@@ -4,12 +4,12 @@ import React, { useEffect } from 'react';
 import ProgressCard from '@/components/progressCard/ProgressCard';
 import { Gathering } from '@/lib/definition';
 import { getInstance } from '@/utils/axios';
-import { buildFilteredParams } from '@/components/progressCard/ProgressCardList';
 
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { QueryKey, useQuery, useQueryClient } from '@tanstack/react-query';
 import useFilterStore from '@/stores/filterStore';
 import useSavedStore from '@/stores/savedStore';
 import useUserStore from '@/stores/userStore';
+import { buildFilteredParams } from '@/utils/gathering';
 
 const SavedProgressCardList = () => {
   const { location, date, sortBy, type } = useFilterStore(); // 필터링 상태 가져오기
@@ -17,36 +17,40 @@ const SavedProgressCardList = () => {
   const { user } = useUserStore();
 
   const savedIds = user ? saved[user.id] : [];
+  const queryClient = useQueryClient();
 
+  const queryKey: QueryKey = ['saved-gathering', location, date, sortBy, type];
   const { data, isLoading, isError, error, refetch } = useQuery({
-    queryKey: ['saved-gathering', location, date, sortBy, type],
+    queryKey,
     queryFn: async () => {
       const params = buildFilteredParams({
         id: savedIds.join(','),
         location: location !== '지역 선택' ? location : undefined,
         date: date !== '날짜 선택' ? date : undefined,
-        sortBy: sortBy === '마감 임박' ? 'dateTime' : 'participantCount', // 마감임박, 참여 인원 순 기본 값 필요
+        sortBy: sortBy === '마감 임박' ? 'dateTime' : 'participantCount',
         type: type !== 'DALLAEMFIT' ? type : undefined,
       });
-      console.log(params);
       const response = await getInstance().get<Gathering[]>('gatherings', {
         params,
       });
       return response.data;
     },
-    enabled: savedIds?.length > 0, // savedIds가 있을 때만 쿼리 실행
+    enabled: savedIds?.length > 0,
   });
+
+  useEffect(() => {
+    if (hydrated && savedIds.length > 0) {
+      queryClient.invalidateQueries({ queryKey });
+    }
+  }, [savedIds]);
 
   useEffect(() => {
     if (hydrated) {
       refetch();
     }
-
-    // if (savedIds) refetch();
-  }, [location, date, sortBy, type, hydrated]); // hydrated를 의존성 배열에 추가
+  }, [location, date, sortBy, type, hydrated]);
 
   if (!hydrated) {
-    // hydrated 상태가 아닐 때 로딩 메시지나 스피너 표시
     return (
       <div className="flex items-center justify-center h-full min-h-500pxr">
         <p className="h-10 text-sm font-medium text-center text-gray-500">데이터 로딩 중...</p>
