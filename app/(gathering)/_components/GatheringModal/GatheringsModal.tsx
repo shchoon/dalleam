70cb8074fc49139cd3824d0a0beb055c88d36535
ohdong9795 <65821619+ohdong9795@users.noreply.github.com>
2024-10-10 13:47:30 +0900

    모임만들기 모달 css, default value 수정 (#40)
    * refactor: update default values in GatheringModal and LoginForm
    
    * refactor: update default values in GatheringModal and LoginForm

--- app/(gathering)/_components/GatheringModal/GatheringsModal.tsx
+++ app/(gathering)/_components/GatheringModal/GatheringsModal.tsx
@@ -21,7 +21,11 @@ export default function GatheringModal({ onClose }: { onClose: () => void }) {
     control,
     handleSubmit,
     formState: { isValid },
-  } = useForm<gatheringSchema>();
+  } = useForm<gatheringSchema>({
+    defaultValues: {
+      capacity: 5,
+    },
+  });
   const onSubmitHandler: SubmitHandler<gatheringSchema> = async (gathering) => {
     mutate.mutate({ gathering });
   };
@@ -55,7 +59,7 @@ export default function GatheringModal({ onClose }: { onClose: () => void }) {
   });
 
   return (
-    <div className="scrollbar w-dvw h-full md:w-520pxr px-4 pt-6 pb-3 md:pb-6 md:px-6 bg-white overflow-auto flex flex-col">
+    <div className="scrollbar w-dvw h-dvh md:h-[96vh] lg:h-[100vh] md:w-520pxr px-4 pt-6 pb-3 md:pb-6 md:px-6 bg-white overflow-auto flex flex-col">
       <form
         onSubmit={handleSubmit(onSubmitHandler)}
         className="w-full flex-grow flex flex-col gap-6"
