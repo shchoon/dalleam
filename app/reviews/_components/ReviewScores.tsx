b63f2dd14877f9857b9dfe60370269b2004dc4e5
Namyunha <108773845+Namyunha@users.noreply.github.com>
2024-10-11 14:23:28 +0900

    feat: 지연 로딩

--- app/reviews/_components/ReviewScores.tsx
+++ app/reviews/_components/ReviewScores.tsx
@@ -6,6 +6,8 @@ import { useMotionValue, useTransform, animate, motion } from 'framer-motion';
 import { useScoresQuery } from '@/services/reviews';
 import useFilterStore from '@/stores/filterStore';
 import Skeleton from 'react-loading-skeleton';
+import SkeletonScores from './skeletonComponents/SkeletonScores';
+import DeferredComponent from '@/components/DeferredComponent';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function ReviewScores() {
@@ -42,23 +44,18 @@ export default function ReviewScores() {
     return () => controls.stop();
   }, [averageScore, rounded]);
 
-  return (
+  return isLoading ? (
+    <DeferredComponent>
+      <SkeletonScores />
+    </DeferredComponent>
+  ) : (
     <div className="flex w-full h-180pxr justify-center shrink-0 bg-white border-y-2 border-gray-200 my-5">
       <div className="flex h-full w-294pxr justify-between md:w-full md:gap-120pxr md:justify-center items-center">
         {/* 하트 평균 점수 */}
         <div className="flex flex-col items-center gap-2">
           <div className="flex items-start gap-2pxr text-2xl font-semibold text-gray-900">
-            {isLoading ? (
-              <>
-                <Skeleton width="30px" height="24px" />
-                <Skeleton width="30px" height="24px" />
-              </>
-            ) : (
-              <>
-                <motion.span>{Math.round(!averageScore ? 0 : averageScore)}</motion.span>
-                <span className="text-2xl font-semibold text-gray-400">/5</span>
-              </>
-            )}
+            <motion.span>{Math.round(!averageScore ? 0 : averageScore)}</motion.span>
+            <span className="text-2xl font-semibold text-gray-400">/5</span>
           </div>
           <div className="flex items-start gap-2pxr">
             {Array.from({ length: 5 }, (_, idx) =>
@@ -80,29 +77,19 @@ export default function ReviewScores() {
         <div className="flex flex-col items-start gap-4pxr">
           {newStars.toReversed().map((el, idx) => (
             <div key={idx} className="flex items-start gap-3">
-              {isLoading ? (
-                <div className="w-146pxr md:w-302pxr h-20pxr">
-                  <Skeleton height="100%" width="100%" />
-                </div>
-              ) : (
-                <>
-                  <div className="flex items-center gap-3">
-                    <span className="text-sm font-medium text-gray-700">
-                      {newStars.length - idx}점
-                    </span>
-                    <span className="w-84pxr md:w-60 lg:w-72 h-1 bg-gray-300 rounded-md relative">
-                      <motion.span
-                        initial={{ scaleX: 0 }}
-                        animate={{ scaleX: totalReviews ? el / points : 0 }}
-                        transition={{ duration: 0.8 }}
-                        style={{ transformOrigin: 'left' }}
-                        className="absolute top-0 left-0 w-full h-1 bg-black"
-                      ></motion.span>
-                    </span>
-                  </div>
-                  <span className="text-sm font-medium text-gray-400">{el}</span>
-                </>
-              )}
+              <div className="flex items-center gap-3">
+                <span className="text-sm font-medium text-gray-700">{newStars.length - idx}점</span>
+                <span className="w-84pxr md:w-60 lg:w-72 h-1 bg-gray-300 rounded-md relative">
+                  <motion.span
+                    initial={{ scaleX: 0 }}
+                    animate={{ scaleX: totalReviews ? el / points : 0 }}
+                    transition={{ duration: 0.8 }}
+                    style={{ transformOrigin: 'left' }}
+                    className="absolute top-0 left-0 w-full h-1 bg-black"
+                  ></motion.span>
+                </span>
+              </div>
+              <span className="text-sm font-medium text-gray-400">{el}</span>
             </div>
           ))}
         </div>
