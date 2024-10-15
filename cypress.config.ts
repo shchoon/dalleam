454e753ae75bffd73832eabcdb74a8ad29b713d5
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-10-15 11:17:24 +0900

    feat: Cypress.config.ts 환경 변수 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
     },
     baseUrl: 'http://localhost:3000/',
   },
+  env: {
+    apiBaseUrl: 'https://fe-adv-project-together-dallaem.vercel.app/FESI3-3',
+    userId: '1234@naver.com',
+    password: '00000000',
+  },
 });
