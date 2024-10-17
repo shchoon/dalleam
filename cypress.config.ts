f3151932a0bb88dc98eb1dea2b33033d07d91ac7
shcho <acckr0604@gmail.com>
2024-10-17 15:23:06 +0900

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000',
   },
   env: {
     apiBaseUrl: 'https://fe-adv-project-together-dallaem.vercel.app/FESI3-3',
